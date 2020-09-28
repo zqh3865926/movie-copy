ba7f3c03f6f3956e430cae6b78a70c8fc06da811
郑清华 <qinghua.zheng@57dp.com>
2020-09-28 15:11:25 +0800

    修改内容

--- head.php
+++ head.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>可依影视搜索引擎_可依影视采集_可依电影福利院_可依福利电影网_2020最新福利电影电视剧大全在线观看 - u3.ink</title>
-	<meta name="Keywords" content="可依影视搜索引擎,可依福利电影网,可依电影网,可依手机网,可依看福利网,可依影城,可依福利影城网,u3.ink">
-	<meta name="Description" content="可依影视搜索引擎（u3.ink）最干净的免费电影网,给你更方便的高清无广告电影电视剧在线观看,爱看福利影城电影下载,观看高清电影就上可依采集系统!">
+    <title>崋箜影视搜索引擎_崋箜影视采集_崋箜电影福利院_崋箜福利电影网_2020最新福利电影电视剧大全在线观看 - u3.ink</title>
+	<meta name="Keywords" content="崋箜影视搜索引擎,崋箜福利电影网,崋箜电影网,崋箜手机网,崋箜看福利网,崋箜影城,崋箜福利影城网">
+	<meta name="Description" content="崋箜影视搜索最干净的免费电影网,给你更方便的高清无广告电影电视剧在线观看,爱看福利影城电影下载,观看高清电影就上崋箜采集系统!">
     <meta http-equiv="Cache-Control" content="no-siteapp">
     <meta name="referrer" content="no-referrer">
     <link rel="shortcut icon" href="./images/32x32.jpg">
@@ -22,25 +22,25 @@
     <script src="./js/jquery.min.js"></script>
     <script src="./js/jquery.lazyload.min.js"></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138875328-4"></script>
-    <script>
-        if (navigator.serviceWorker) {
-            navigator.serviceWorker.register('./js/sw.js');
-        }
-        
-	  	window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-	  	gtag('js', new Date());
-	
-	  	gtag('config', 'UA-138875328-4');
-		var _hmt = _hmt || [];
-		(function() {
-		  var hm = document.createElement("script");
-		  hm.src = "https://hm.baidu.com/hm.js?2a082bb5cde6292851e66a63ee1595e1";
-		  var s = document.getElementsByTagName("script")[0]; 
-		  s.parentNode.insertBefore(hm, s);
-		})();
-    </script>
+<!--	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138875328-4"></script>-->
+<!--    <script>-->
+<!--        if (navigator.serviceWorker) {-->
+<!--            navigator.serviceWorker.register('./js/sw.js');-->
+<!--        }-->
+<!--        -->
+<!--	  	window.dataLayer = window.dataLayer || [];-->
+<!--		function gtag(){dataLayer.push(arguments);}-->
+<!--	  	gtag('js', new Date());-->
+<!--	-->
+<!--	  	gtag('config', 'UA-138875328-4');-->
+<!--		var _hmt = _hmt || [];-->
+<!--		(function() {-->
+<!--		  var hm = document.createElement("script");-->
+<!--		  hm.src = "https://hm.baidu.com/hm.js?2a082bb5cde6292851e66a63ee1595e1";-->
+<!--		  var s = document.getElementsByTagName("script")[0]; -->
+<!--		  s.parentNode.insertBefore(hm, s);-->
+<!--		})();-->
+<!--    </script>-->
 </head>
 <body>
 	<div id="header">
@@ -57,4 +57,3 @@
 	        <button id="submit"><i class="fas fa-search"></i></button>
 	       </form>
 	    </div>
-	
